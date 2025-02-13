357d2330a0a5334e8d6073e9595d4de960875df9
omaromarsalhi <omar.salhi.job@gmail.com>
2025-02-13 21:05:53 +0100

    first commit

--- fontOffice/src/app/app-routing.module.ts
+++ fontOffice/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { InsurancesComponent } from './insurance/insurances/insurances.component'
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent }, // Default route
+  { path: 'insurances', component: InsurancesComponent }, // Navigate to 'About'
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
