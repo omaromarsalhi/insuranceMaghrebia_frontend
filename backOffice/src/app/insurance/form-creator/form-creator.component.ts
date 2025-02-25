65ad383b9898c2dd54af08e7b543d7226b77a9e8
omaromarsalhi <omar.salhi.job@gmail.com>
2025-02-25 20:25:10 +0100

    finished adding input controle with regex

--- backOffice/src/app/insurance/form-creator/form-creator.component.ts
+++ backOffice/src/app/insurance/form-creator/form-creator.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit, EventEmitter, Output } from "@angular/core";
-import { FormBuilder, FormGroup, FormArray, FormControl, Validators, AbstractControl } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormArray,
+  FormControl,
+  Validators,
+  AbstractControl,
+  ValidationErrors,
+} from "@angular/forms";
 import { CdkDragDrop, moveItemInArray } from "@angular/cdk/drag-drop";
 import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 import { OfferFormData } from "src/app/core/models/insurance/offer-form-data.interface";
@@ -46,7 +54,7 @@ export class FormCreatorComponent implements OnInit {
 
   private initForm(): void {
     this.dynamicForm = this.fb.group({
-      fields: this.fb.array([])
+      fields: this.fb.array([]),
     });
     this.addField();
   }
@@ -61,25 +69,120 @@ export class FormCreatorComponent implements OnInit {
 
   // Type-safe field control accessor
   getFieldControl(index: number, controlName: string): AbstractControl {
-    return (this.fields.at(index) as FormGroup).get(controlName) as AbstractControl;
+    return (this.fields.at(index) as FormGroup).get(
+      controlName
+    ) as AbstractControl;
   }
 
+  // createField(order: number): FormGroup {
+  //   return this.fb.group({
+  //     label: ["", Validators.required],
+  //     type: ["text", Validators.required],
+  //     order: [order, [Validators.required, Validators.min(1)]],
+  //     required: [false],
+  //     placeholder: ["",Validators.required],
+  //     description: ["",Validators.required],
+  //     regex: ["",Validators.required],
+  //     regexErrorMessage: [""],
+  //     rangeStart: [0],
+  //     rangeEnd: [10],
+  //     selectOptions: this.fb.array([])
+  //   });
+  // }
+
+  // Custom Validators
+
+  validRegex(control: AbstractControl): ValidationErrors | null {
+    if (!control.value) return null;
+    try {
+      new RegExp(control.value);
+      return null;
+    } catch (e) {
+      return { invalidRegex: true };
+    }
+  }
+
+  validateRange(group: FormGroup): ValidationErrors | null {
+    const start = group.get("rangeStart")?.value;
+    const end = group.get("rangeEnd")?.value;
+    return start !== null && end !== null && start > end
+      ? { invalidRange: true }
+      : null;
+  }
+
+  validateSelectOptions(group: FormGroup): ValidationErrors | null {
+    const type = group.get("type")?.value;
+    const options = group.get("selectOptions") as FormArray;
+    return type === "select" && options.length === 0
+      ? { selectOptionsRequired: true }
+      : null;
+  }
+
+  // Updated createField function
   createField(order: number): FormGroup {
+    return this.fb.group(
+      {
+        label: [
+          "",
+          [
+            Validators.required,
+            Validators.pattern(/^[a-zA-Z0-9\s\-.,'()]{1,100}$/),
+          ],
+        ],
+        type: [
+          "text",
+          [
+            Validators.required,
+            Validators.pattern(
+              /^(text|email|number|password|date|tel|url|select|checkbox|radio)$/
+            ),
+          ],
+        ],
+        order: [
+          order,
+          [
+            Validators.required,
+            Validators.min(1),
+            Validators.pattern(/^[1-9]\d*$/),
+          ],
+        ],
+        required: [false],
+        placeholder: [
+          "",
+          [Validators.required, Validators.pattern(/^[\w\s\-.,'()]{1,100}$/)],
+        ],
+        description: [
+          "",
+          [Validators.required, Validators.pattern(/^[\w\s\-.,'()]{0,500}$/)],
+        ],
+        regex: ["", [Validators.required, this.validRegex]],
+        regexErrorMessage: [
+          "",
+          [Validators.required,Validators.pattern(/^[a-zA-Z0-9\s\-.,!?'()]{0,200}$/)],
+        ],
+        rangeStart: [0, [Validators.pattern(/^-?\d+$/)]],
+        rangeEnd: [10, [Validators.pattern(/^-?\d+$/)]],
+        selectOptions: this.fb.array([]),
+      },
+      {
+        validators: [this.validateRange, this.validateSelectOptions],
+      }
+    );
+  }
+
+  // Select Options FormArray (add this to your component)
+  createSelectOption(): FormGroup {
     return this.fb.group({
-      label: ["", Validators.required],
-      type: ["text", Validators.required],
-      order: [order, [Validators.required, Validators.min(1)]],
-      required: [false],
-      placeholder: ["",Validators.required],
-      description: ["",Validators.required],
-      regex: ["",Validators.required],
-      regexErrorMessage: [""],
-      rangeStart: [0],
-      rangeEnd: [10],
-      selectOptions: this.fb.array([])
+      value: [
+        "",
+        [Validators.required, Validators.pattern(/^[a-zA-Z0-9\s\-]{1,50}$/)],
+      ],
+      label: [
+        "",
+        [Validators.required, Validators.pattern(/^[a-zA-Z0-9\s\-]{1,50}$/)],
+      ],
     });
   }
-
 
   getSelectOptions(field: FormGroup): FormArray {
     return field.get("selectOptions") as FormArray;
@@ -149,8 +252,6 @@ export class FormCreatorComponent implements OnInit {
       .map((control) => control as FormGroup)
       .sort((a, b) => a.get("order")?.value - b.get("order")?.value);
   }
-
-
 
   removeSelectOption(field: FormGroup, index: number): void {
     this.getSelectOptions(field).removeAt(index);
